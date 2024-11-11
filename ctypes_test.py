a9c2813aef6d69d27e8c8b14da59ba693576e123
Jessica Tang <jessicatang2019@gmail.com>
2024-11-11 09:57:44 -0500

    python motive API communications test

---
+++ ctypes_test.py
@@ -0,0 +1,27 @@
+'''
+Simple tester for the Motive API using ctypes
+'''
+
+import ctypes
+import time
+
+
+# Load the Motive API DLL
+motive_api = ctypes.WinDLL("C:/Program Files/OptiTrack/Motive/lib/MotiveAPI.dll")
+
+# Do we need to load a Motive camera calibration file?
+
+motive_api.Initialize()
+print("motive API initialized")
+
+motive_api.StartRecording()
+print("recording started")
+
+time.sleep(5) # wait 5 seconds
+
+motive_api.StopRecording()
+print("recording stopped")
+
+# Shutdown Motive SDK
+motive_api.Shutdown()
+print("motive API shutdown")
